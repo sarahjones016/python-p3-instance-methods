8b36e2bb70caae2e5a22a2bb5d26a42f04604c01
Sarah Jones <sarahrjones8@gmail.com>
2023-05-02 10:29:27 -0400

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,12 @@
 class Person:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+sarah = Person()
+sarah.talk()
+sarah.walk()
